99599f22f33916fd894a67cb2670aa6e836becd7
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2017-02-05 17:08:16 +0800

    initial commit

---
+++ assets/js/amp-img-generator.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    width: '',
+    height: '',
+    layout: 'responsive',
+    alt: '',
+    src: ''
+  },
+  methods: {
+    getImageData: function () {
+        var img = new Image();
+        img.onload = function(){
+            // ambil ukuran gambar
+            app.width = this.width
+            app.height = this.height
+        };
+        img.src = this.src;
+        console.log(this.src);
+
+        var index = this.src.lastIndexOf("/") + 1;
+        var filename = this.src.substr(index);
+        this.alt = unescape(filename);
+        console.log(filename);
+    }
+  }
+})
